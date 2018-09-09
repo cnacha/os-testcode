71a8d8a747ade208bc1ecbdc8f4171615838f9de
cnacha <cnacha@gmail.com>
2018-09-10 09:59:12 +1200

    update Robert's changes

--- testsemaphore.c
+++ testsemaphore.c
@@ -1,15 +1,15 @@
-// C program to demonstrate working of Semaphores
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t mutex;
+sem_t semaphore;
 
 void* thread(void* arg)
 {
     //wait
-    sem_wait(&mutex);
+    printf("\nBefore the wait.\n");
+    sem_wait(&semaphore);
     printf("\nEntered..\n");
 
     //critical section
@@ -17,20 +17,20 @@ void* thread(void* arg)
     
     //signal
     printf("\nJust Exiting...\n");
-    sem_post(&mutex);
+    sem_post(&semaphore);
+    printf("\nAfter the post.\n");
 }
 
 
 int main()
 {
-    sem_init(&mutex, 0, 1);
+    sem_init(&semaphore, 0, 2);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,thread,NULL);
     sleep(2);
     pthread_create(&t2,NULL,thread,NULL);
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
-    sem_destroy(&mutex);
+    sem_destroy(&semaphore);
     return 0;
 }
-
