3d7047fe25b72f3314f28701d17845a18316cf9c
cnacha <cnacha@gmail.com>
2018-09-19 11:11:47 +1200

    update semaphore

--- test0_mod.c
+++ test0_mod.c
@@ -7,10 +7,14 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t mutex;
+sem_t semaphore;
+
+volatile long counter = 0;
 
 void test0() {
-    sleep(1);
+   //sleep(1);
+   for (int i = 0; i < 1000000000; i++)
+        counter++;
     printf("test running\n");
 }
 
@@ -26,17 +30,17 @@ task_t *task_create(void (*work)(void *), void *params, char *name) {
 
 void *dummy_runner(void *run_task) {
     task_t *my_task = (task_t *)run_task;
-    sem_wait(&mutex);
+    sem_wait(&semaphore);
     // critical section begins here
     printf("starting dummy_runner on %s\n", my_task->name);
     my_task->work(NULL); /* possibly some parameters later */
     printf("finished dummy_runner on %s\n", my_task->name);
     // critical section ends here
-    sem_post(&mutex);
+    sem_post(&semaphore);
 }
 
 int main(int argc, char** argv) {
-    sem_init(&mutex, 0, 1);
+    sem_init(&semaphore, 0, 1);
 
     task_t *task1, *task2;
     pthread_t test_thread1, test_thread2;
